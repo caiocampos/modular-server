1f98b808b59a74c36721b51788048d11bf47168c
caiocampos <20524962+caiocampos@users.noreply.github.com>
2024-11-02 13:27:47 +0000

    Deploying to dist from @ caiocampos/modular-server@64fa15f0696e67e835648bc96340fd0e98fbc24b 🚀

---
+++ main.js
@@ -0,0 +1,114 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/common");
+
+/***/ }),
+/* 2 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/core");
+
+/***/ }),
+/* 3 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.AppModule = void 0;
+const common_1 = __webpack_require__(1);
+const config_1 = __webpack_require__(4);
+const modules_expose_1 = __webpack_require__(5);
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [config_1.ConfigModule.forRoot(), ...modules_expose_1.moduleList],
+    })
+], AppModule);
+
+
+/***/ }),
+/* 4 */
+/***/ ((module) => {
+
+module.exports = require("@nestjs/config");
+
+/***/ }),
+/* 5 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.moduleList = void 0;
+const AsteroidScoreModules = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module './modules/asteroid-score/modules-expose'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+const BlogPostsModules = __webpack_require__(Object(function webpackMissingModule() { var e = new Error("Cannot find module './modules/blog-posts/modules-expose'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+exports.moduleList = [...AsteroidScoreModules.moduleList, ...BlogPostsModules.moduleList];
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const common_1 = __webpack_require__(1);
+const core_1 = __webpack_require__(2);
+const app_module_1 = __webpack_require__(3);
+const bootstrap = async () => {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
+    app.useGlobalPipes(new common_1.ValidationPipe());
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+        next();
+    });
+    await app.listen(3000);
+};
+bootstrap();
+
+})();
+
+/******/ })()
+;
