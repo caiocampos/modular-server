6445b92b54a755e2bb9829d397eea9d69e89209b
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2024-11-04 23:29:24 -0300

    chore: automaticaly update dist.json

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-rm src/modules-expose.ts
-cp src/modules-expose.txt src/modules-expose.ts
-
 yarn build:webpack
 
 echo "require(\"./main\");" > dist/index.js
@@ -10,5 +7,7 @@ echo "require(\"./main\");" > dist/index.js
 echo "# .gitignore
 node_modules/
 package-lock.json" > dist/.gitignore
+
+node generate-dist-json.js
 
 cp dist.json dist/package.json
