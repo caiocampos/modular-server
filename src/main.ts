d5e18da8092a09d00e3616cb0eafe72815dd5d80
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2024-12-18 19:29:52 -0300

    chore: changes to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -13,12 +13,6 @@ const bootstrap = async () => {
   );
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
   await app.listen(process.env.PORT ?? 3000);
 };
 bootstrap();
