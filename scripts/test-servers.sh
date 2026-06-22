4a144570697212f5966941214259505d6e5c1105
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2026-06-22 20:39:06 -0300

    Update test-servers.sh

--- scripts/test-servers.sh
+++ scripts/test-servers.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 sleep_random () {
-  SLEEP_TIME=$((10 + RANDOM % 21));
-  echo "sleeping ${SLEEP_TIME}s";
+  SLEEP_TIME=$((1 + RANDOM % 5));
   sleep $SLEEP_TIME;
 };
 
 test_server () {
     NAME=$1;
     URL=$2;
+    SLEEP=$3;
+    if [ $SLEEP -eq 1 ]; then
+      sleep_random;
+    fi
     RESPONSE=$(curl -s ${URL});
     echo "response $NAME -> $RESPONSE";
-    sleep_random;
 };
 
 test_servers () {
@@ -27,14 +29,18 @@ test_servers () {
   );
   HITS=("" "" "");
   for i in `seq 1 $((1 + RANDOM % 4))`; do
+    SLEEP=1;
+    if [ $i -eq 1 ]; then
+      SLEEP=0;
+    fi
     SELECTED=$((RANDOM % ${#URLS[@]}));
-    test_server ${NAMES[$SELECTED]} ${URLS[$SELECTED]};
+    test_server ${NAMES[$SELECTED]} ${URLS[$SELECTED]} $SLEEP;
     HITS[$SELECTED]="HIT";
   done
   echo
   for i in `seq 0 $((${#URLS[@]} - 1))`; do
     if [ -z ${HITS[$i]} ]; then
-      test_server ${NAMES[$i]} ${URLS[$i]};
+      test_server ${NAMES[$i]} ${URLS[$i]} 1;
     fi
   done
 };
