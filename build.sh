3dd4b1e963916ce53281216b1522d4f393b7e553
Caio Campos <20524962+caiocampos@users.noreply.github.com>
2026-09-01 20:45:12 -0300

    fix to build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cp src/modules-expose.txt src/modules-expose.ts
 
 yarn build:rspack
 
-echo "await import './main.js';" > dist/index.js
+echo "(async () => { await import './main.js'; })()" > dist/index.js
 
 echo "# .gitignore
 node_modules/
